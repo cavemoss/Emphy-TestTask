019f5998925311ac6daea62cd13d67330163104b
mossy boy <alexshu1812@gmail.com>
2024-10-02 20:59:55 +0800

    modify readme

--- README.md
+++ README.md
@@ -4,12 +4,13 @@
 - Обойти CORS через локальный proxy сервер.
 - Вывести данные по существующим сделкам на главной странице проекта.
 
+Выполнено с использованием `Express.js` `HTML` и `JQuery`
+
+
+## Инструкция
+
 `npm install` Установить dependencies
 
-`npm run rdr` Запустить тестовый redirect_server (http://localhost:3000)
+`npm run prx` Запустить локальный proxy сервер (http://localhost:8080)
 
-`npm run rdr` Запустить тестовый proxy_server (http://localhost:8080)
-
-Открыть `./src/main-page.html` через  VS Code Live Server или через команду `serve ./src/main-page.html`
-
-`SUB_DOMAIN`, `CLIENT_ID`, `CLIENT_SECRET` и `CODE` необходимо указать в файле `.env`
+Открыть `./src/index.html` через  VS Code Live Server или через команду `serve ./src`
--- proxy_server.js
+++ proxy_server.js
@@ -7,31 +7,12 @@ const app = express()
 dotenv.config()
 app.use(cors())
 
-try // Acquiring the access token
-{
-    const response = await axios.post(`https://${process.env.SUB_DOMAIN}.amocrm.ru/oauth2/access_token`, {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        code: process.env.CODE,
-        grant_type: "authorization_code",
-        redirect_uri: "http://localhost:3000/redirect"
-    })
-    var ACCESS_TOKEN = response.data.access_token
-    console.log('access_token acquired')
-}
-catch (exception) { 
-    if(exception.status) console.log('code expired')
-    else console.log('network error')
-    process.exit(0)
-}
-
+const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImMyMWQ3MmFmZDZkZmMxZjE2ZGE5MWNiOGUwOTM2ZWRhMDE2ZWI5YWUzZjcwZmUyNWUyZjY4YWFlNzMxNjhkMTcyMDgxMTE2MjUyZTNlZTlmIn0.eyJhdWQiOiJjMWM0MjQ5NS1hMTczLTQzOWYtOGI3OS0xNTEzZWU3OWVkOGQiLCJqdGkiOiJjMjFkNzJhZmQ2ZGZjMWYxNmRhOTFjYjhlMDkzNmVkYTAxNmViOWFlM2Y3MGZlMjVlMmY2OGFhZTczMTY4ZDE3MjA4MTExNjI1MmUzZWU5ZiIsImlhdCI6MTcyNzg3MzE1NCwibmJmIjoxNzI3ODczMTU0LCJleHAiOjE3Mjc5NTk1NTQsInN1YiI6IjExNTkyMDAyIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxOTgzNDU0LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiNjczZDkwYTMtZTZkYy00YTNkLWFhMzctMDY0NjBlMzllMmE1IiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.lQBUfmcZgkaWoPqxzaZPsrQLJWeLIB__kUnnm6jxNDVzLkNfrXPU7daGgiZ_5Pkq5AJ45-0F-r3AAEtXGOWjriyo7d7CilZK865qjo2KY-KaZs6orIKGNqqk73sOkDttyvNJeO2epLzTA0Hze-w2NiSWTHOrUHLXUC3SVJ8W_eNNW_dlyRuJS2bZ277MyAdO02pOoJlpOTPPbe0Yt2bhViz_d9-AbinsThok5X6hPefqUBiecjKRADZlr8WK35AZ_FOrfv7TMGK8h_5VZFcepdmFGzSLLT1vRFSwoSDRcmDER3wKQA-iHIMraJpoOTbcJZNUtlxXDJQnDP8dvAG-og"
 
 // http://localhost:8080/api/leads
 app.get('/api/leads', async function (req, res) {
-    if (!ACCESS_TOKEN) return res.status(500).send('Internal Server Error')
-    try
-    {
-        const response = await axios.get(`https://${process.env.SUB_DOMAIN}.amocrm.ru/api/v4/leads?page=${req.query.page}&limit=3`, {
+    try {
+        const response = await axios.get(`https://alexshu1812.amocrm.ru/api/v4/leads?page=${req.query.page}&limit=3`, {
             headers: { 'Authorization': `Bearer ${ACCESS_TOKEN}` }
         })
         res.json(response.data)
@@ -41,10 +22,8 @@ app.get('/api/leads', async function (req, res) {
 
 // http://localhost:8080/api/full-info
 app.get('/api/full-info', async function (req, res) {
-    if (!ACCESS_TOKEN) return res.status(500).send('Internal Server Error')
-    try 
-    {
-        const response = await axios.get(`https://${process.env.SUB_DOMAIN}.amocrm.ru/api/v4/tasks?filter[entity_type]=leads&filter[entity_id]=${req.query.id}`, {
+    try {
+        const response = await axios.get(`https://alexshu1812.amocrm.ru/api/v4/tasks?filter[entity_type]=leads&filter[entity_id]=${req.query.id}`, {
             headers: { 'Authorization': `Bearer ${ACCESS_TOKEN}` }
         })
         res.json(response.data)
